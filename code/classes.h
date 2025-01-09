024dcf0fe473e5e65e464c81d7b8e3fef2f4bcf0
mzavarsk <marc.zavarsky@edu.univ-fcomte.fr>
2025-01-09 14:59:06 +0100

    Trying out views and entities

--- code/classes.h
+++ code/classes.h
@@ -1,71 +1,79 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <vector>
+// #include <iostream>
+// #include <string>
+// #include <cmath>
+// #include <vector>
+// #include <gf/Direction.h>
+// #include <gf/Entity.h>
+// #include <gf/Font.h>
+// #include <gf/Random.h>
+// #include <gf/Rect.h>
 
-enum class Role {
-    Attacker,
-    Defender,
-    Goalkeeper
-};
+// enum class Role {
+//     ATTACKER,
+//     DEFENDER,
+//     GOALKEEPER
+// };
 
-float calculateDistance(float x, float y, float x2, float y2);
+// float calculateDistance(float x, float y, float x2, float y2);
 
-class Player {
-private:
-    float stamina;
-    float speed;
-    float x, y;
-    Role role;
-public:
-    /** @brief Construct a new Player object*/
-    Player(float stamina, float speed, float x, float y, Role role);
+// class Player {
+// private:
+//     float m_stamina; //Pas sûr pour le moment
+//     float m_speed;
+//     float m_size;
+//     gf::Vector2f m_position;
+//     Role m_role;
+// public:
+//     /** @brief Construct a new Player object*/
+//     Player(float stamina, float speed, gf::Vector2f position, Role role);
 
-    /** @brief Getters */
-    // idk if we need all of these
-    float getStamina();
-    float getSpeed();
-    float getX();
-    float getY();
-    Role getRole();
+//     /** @brief Getters */
+//     // idk if we need all of these
+//     float getStamina();
+//     float getSpeed();
+//     float getX();
+//     float getY();
+//     Role getRole();
 
-    /** @brief Setters */
-    // idk if needed
-    void setStamina(float stamina);
-    void setSpeed(float speed);
-    void setX(float x);
-    void setY(float y);
-    void setRole(Role role);
+//     /** @brief Setters */
+//     // idk if needed
+//     void setStamina(float stamina);
+//     void setSpeed(float speed);
+//     void setX(float x);
+//     void setY(float y);
+//     void setRole(Role role);
 
-    void move(float dx, float dy); // maybe add speed here?
-    void shoot(float power, float angle); // idk? maybe have speed of the player impact this? :3
 
-    void display();
 
-};
+//     void move(float dx, float dy); // maybe add speed here?
+//     void shoot(float power, float angle); // idk? maybe have speed of the player impact this? :3
 
-class Ball {
-private:
-    float x, y;
-public:
-    Ball(float x, float y);
+//     void display();
 
-    float getX();
-    float getY();
+// };
 
-    void move(float dx, float dy);
-    void display();
+// class Ball {
+// private:
+//     float x, y;
+// public:
+//     Ball(float x, float y);
 
-    // do we need the speed of the ball? maybe?
-};
+//     float getX();
+//     float getY();
 
-class Field {
-private:
-    float width, height;
-    std::vector<std::pair<float, float>> goals;
-public:
-    Field(float width, float height);
-    bool isGoal(float x, float y);
-    bool isOutOfBounds(float x, float y);
-};
+//     void move(float dx, float dy);
+//     void display();
+
+//     // do we need the speed of the ball? maybe?
+// };
+
+// class Field {
+// private:
+//     float width, height;
+//     std::vector<std::pair<float, float>> goals;
+// public:
+//     Field(float width, float height);
+//     bool isGoal(float x, float y);
+//     bool isOutOfBounds(float x, float y);
+// };
 
--- code/football.cc
+++ code/football.cc
@@ -1,8 +1,3 @@
-// #include <iostream>
-// #include <string>
-// #include <cmath>
-// #include <vector>
-// #include "classes.h"
 
 
 // float calculateDistance(float x, float y, float x2, float y2){
@@ -87,6 +82,11 @@
 #include <cassert>
 #include <cstdio>
 
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include "classes.h"
 #include <gf/Action.h>
 #include <gf/Clock.h>
 #include <gf/Color.h>
@@ -102,12 +102,121 @@
 #include <gf/Random.h>
 #include <gf/Shapes.h>
 #include <gf/Collision.h>
+#include <gf/Entity.h>
 
 #include "config.h"
 
 #define FOOTBALL_DATA_DIR "@FOOTBALL_DATA_DIR@"
 
-static constexpr float Speed = 60.0f;
+static constexpr float SPEED = 60.0f;
+
+enum class Role {
+    ATTACKER,
+    DEFENDER,
+    GOALKEEPER
+};
+
+class Player : public gf::Entity {
+  private:
+    float m_stamina; //Pas sûr pour le moment
+    gf::Vector2f m_velocity;
+    float m_size;
+    gf::Vector2f m_position;
+    Role m_role;
+    gf::Color4f m_color;
+  public : 
+    Player(float stamina, float size, gf::Vector2f position, Role role, gf::Color4f color) {
+      m_stamina = stamina;
+      m_velocity = {0,0};
+      m_size = size;
+      m_position = position;
+      m_role = role;
+    }
+
+    float getSize() {
+      return m_size;
+    }
+
+    gf::Vector2f getPosition() {
+      return m_position;
+    }
+
+    void setVelocity(gf::Vector2f velocity) {
+      m_velocity = velocity;
+    }
+
+    void update(float dt) {
+      m_position += dt * m_velocity;
+    }
+
+    void render(gf::RenderTarget& target) {
+      gf::RectangleShape shape({m_size, m_size});
+      shape.setPosition(m_position);
+      shape.setAnchor(gf::Anchor::TopLeft);
+      shape.setOutlineThickness(0.5f);
+      // shape.setColor(m_color);
+      // shape.setOutlineColor(gf::Color::darker(m_color));
+      shape.setColor(gf::Color::Azure);
+      shape.setOutlineColor(gf::Color::darker(gf::Color::Azure));
+      target.draw(shape);
+    }
+};
+
+class Ball : public gf::Entity {
+  private:
+    gf::Vector2f m_velocity;
+    float m_size;
+    gf::Vector2f m_position;
+    gf::Color4f m_color;
+    Player *belongsTo;
+  public : 
+    Ball(float size, gf::Vector2f position, gf::Color4f color) {
+      m_velocity = {0,0};
+      m_size = size;
+      m_position = position;
+      belongsTo = nullptr;
+    }
+
+    gf::Vector2f getPosition() {
+      return m_position;
+    }
+
+    float getSize() {
+      return m_size;
+    }
+
+    void setVelocity(gf::Vector2f velocity) {
+      m_velocity = velocity;
+    }
+
+    void update(float dt) {
+      m_position += dt * m_velocity;
+    }
+
+    void lockTo(Player *p) {
+      belongsTo = p;
+    }
+
+    void unlock() {
+      belongsTo = nullptr;
+    }
+
+    bool isLockedTo() {
+      return belongsTo != nullptr;
+    }
+
+    void render(gf::RenderTarget& target) {
+      gf::CircleShape shape(m_size);
+      shape.setPosition(m_position);
+      // shape.setColor(m_color);
+      shape.setColor(gf::Color::Rose);
+      shape.setAnchor(gf::Anchor::TopLeft);
+      shape.setOutlineThickness(0.5f);
+      // shape.setOutlineColor(gf::Color::lighter(m_color));
+      shape.setOutlineColor(gf::Color::lighter(gf::Color::Rose));
+      target.draw(shape);
+    }
+};
 
 int main() {
   static constexpr gf::Vector2u ScreenSize(500, 500);
@@ -132,6 +241,10 @@ int main() {
   // add cameras
 
   gf::ViewContainer views;
+
+  gf::ExtendView view({0.0f, 20.0f}, {500.0f, 500.0f});
+
+  views.addView(view);
 
   views.setInitialFramebufferSize(ScreenSize);
 
@@ -170,32 +283,41 @@ int main() {
   downAction.addScancodeKeyControl(gf::Scancode::Down);
   actions.addAction(downAction);
 
+  gf::Action dropAction("Drop");
+  dropAction.addScancodeKeyControl(gf::Scancode::X);
+  actions.addAction(dropAction);
+
+  gf::Action switchAction("Switch");
+  switchAction.addScancodeKeyControl(gf::Scancode::C);
+  actions.addAction(switchAction);
+
   // add entities
 
   gf::EntityContainer mainEntities;
 
-  // main loop
-
   renderer.clear(gf::Color::fromRgba32(0x7CFC00FF));
+  
 
   gf::Clock clock;
   bool fullscreen = false;
 
+  Player player1(100.0f, 20.0f, {0.0f, 20.0f}, Role::ATTACKER, gf::Color::Azure);
 
-  gf::RectangleShape player({20.0f, 20.0f});
-  player.setColor(gf::Color::Azure);
-  player.setPosition({ 0.0f, 20.0f });
-  player.setOutlineThickness(0.5f);
-  player.setOutlineColor(gf::Color::darker(gf::Color::Azure));
+  mainEntities.addEntity(player1);
 
-  gf::CircleShape ball(10.0f);
-  ball.setPosition({ 200.0f, 20.0f });
-  ball.setColor(gf::Color::Rose);
-  ball.setOutlineThickness(1.0f);
-  ball.setOutlineColor(gf::Color::lighter(gf::Color::Rose));
+  Player player2(100.0f, 20.0f, {400.0f, 20.0f}, Role::ATTACKER, gf::Color::Red);
 
-  bool isMoving = false;
+  mainEntities.addEntity(player2);
+
+  Ball ball(10.0f, {200.0f, 20.0f}, gf::Color::Rose);
+
+  mainEntities.addEntity(ball);
+  
   gf::Vector2f velocity(0.0f, 0.0f);
+
+  // main loop
+
+  bool cam1 = true;
 
   while (window.isOpen()) {
     // input
@@ -216,62 +338,88 @@ int main() {
     }
 
     if ((leftAction.isActive() && rightAction.isActive()) || (!leftAction.isActive() && !rightAction.isActive())) {
-      isMoving = false;
       velocity.x = 0;
     }else {
       if (leftAction.isActive()) {
-        isMoving = true;
-        velocity.x = -Speed;
+        velocity.x = -SPEED;
       } else if (rightAction.isActive()) {
-        isMoving = true;
-        velocity.x = Speed;
+        velocity.x = SPEED;
       }
     }
 
     if ((upAction.isActive() && downAction.isActive()) || (!upAction.isActive() && !downAction.isActive())) {
-      isMoving = isMoving;
       velocity.y = 0;
     }else {
       if (upAction.isActive()) {
-        isMoving = true;
-        velocity.y = -Speed;
+        velocity.y = -SPEED;
       } else if (downAction.isActive()) {
-        isMoving = true;
-        velocity.y = Speed;
+        velocity.y = SPEED;
       }
     }
 
     // update
-    gf::Time time = clock.restart();
-    gf::Vector2f oldPlayerPosition = player.getPosition();
-    if (isMoving) {
-      player.setPosition({oldPlayerPosition.x + (velocity.x * time.asSeconds()), oldPlayerPosition.y + (velocity.y * time.asSeconds())});
-      
-      gf::Vector2f playerPosition = player.getPosition();
-      gf::Vector2f ballPosition = ball.getPosition();
-      
-      float playerLeft = playerPosition.x;
-      float playerRight = playerPosition.x + player.getSize().x;
-      float playerTop = playerPosition.y;
-      float playerBottom = playerPosition.y + player.getSize().y;
 
-      float ballLeft = ballPosition.x;
-      float ballRight = ballPosition.x + ball.getRadius() * 2; // Diamètre
-      float ballTop = ballPosition.y;
-      float ballBottom = ballPosition.y + ball.getRadius() * 2; // Diamètre
-
-      bool isColliding = (playerRight > ballLeft && playerLeft < ballRight &&
-                          playerBottom > ballTop && playerTop < ballBottom);
-
-      if (isColliding) {
-        ball.setPosition({ballPosition.x + (velocity.x * time.asSeconds()), ballPosition.y + (velocity.y * time.asSeconds())});
-      }
+    if (dropAction.isActive()) {
+      ball.unlock();
     }
+
+    if (switchAction.isActive()) {
+      if (cam1) {
+        view.setCenter(player2.getPosition());
+      }else {
+        view.setCenter(player1.getPosition());
+      }
+      cam1 = !cam1;
+    }
+
+    gf::Time dt = clock.restart();
+    player1.setVelocity(velocity);
+
+    player1.update(dt.asSeconds());
+    
+    gf::Vector2f player1Position = player1.getPosition();
+    gf::Vector2f ballPosition = ball.getPosition();
+
+    float player1Size = player1.getSize();
+    float ballSize = player1Size;
+    
+    float player1Left = player1Position.x;
+    float player1Right = player1Position.x + player1Size;
+    float player1Top = player1Position.y;
+    float player1Bottom = player1Position.y + player1Size;
+
+    float ballLeft = ballPosition.x;
+    float ballRight = ballPosition.x + ballSize; // Diamètre
+    float ballTop = ballPosition.y;
+    float ballBottom = ballPosition.y + ballSize; // Diamètre
+
+    bool isColliding = (player1Right > ballLeft && player1Left < ballRight &&
+                        player1Bottom > ballTop && player1Top < ballBottom);
+
+    if (isColliding) {
+      ball.lockTo(&player1);
+    }
+
+    if (ball.isLockedTo()) {
+      ball.setVelocity(velocity);
+    }else {
+      ball.setVelocity({0.0f, 0.0f});
+    }
+
+    // mainEntities.update(dt);
+    player2.update(dt.asSeconds());
+    ball.update(dt.asSeconds());
 
     // render
     renderer.clear();
-    renderer.draw(player);
-    renderer.draw(ball);
+
+    view.move(velocity * dt.asSeconds());
+
+    renderer.setView(view);
+    ball.render(renderer);
+    player1.render(renderer);
+    player2.render(renderer);
+    // mainEntities.render(rend erer);
     renderer.display();
 
   }
